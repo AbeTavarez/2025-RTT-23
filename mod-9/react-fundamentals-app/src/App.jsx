b2c7670886d1525f975ababd819557996b0aa660
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-06-06 13:06:22 -0400

    Mod9 React Fundamentals Lesson 1

---
+++ mod-9/react-fundamentals-app/src/App.jsx
@@ -0,0 +1,32 @@
+// import './App.css'
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+      <main>
+        <h1>React Fundamentals App</h1>
+
+        <section>
+          <h2>Latest Products</h2>
+
+          <p>Checkout our new products</p>
+        </section>
+
+        <section>
+          <h2>Register for a discount</h2>
+          <a href="#">Click here</a>"
+        </section>
+      </main>
+      <footer>
+        <h3>Find us on Facebook</h3>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
