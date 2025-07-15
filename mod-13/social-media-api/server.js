72bf2d02334d4744cf5cbd6f3fac52b0b9a61b66
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-07-15 11:59:29 -0400

    Posts route

--- mod-13/social-media-api/server.js
+++ mod-13/social-media-api/server.js
@@ -1,8 +1,10 @@
 import express from "express";
-// import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import User from "./models/User.js";
+import usersRouter from "./routes/users.js";
+import postsRouter from "./routes/posts.js";
+
+
 
 dotenv.config();
 
@@ -12,6 +14,8 @@ const uri = process.env.MONGODB_URL;
 
 // ====== middleware =======
 app.use(express.json());
+app.use('/api/users', usersRouter);
+app.use('/api/posts', postsRouter);
 
 // Connect to MongoDB using Mongoose
 mongoose
@@ -22,42 +26,6 @@ mongoose
 // ======= Routes ===========
 app.get("/", async (req, res) => {
   res.status(200).json({ message: "Successfully connected to the database!" });
-});
-
-// create a new user
-app.post("/users", async (req, res) => {
-  try {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ error: e.message });
-  }
-});
-
-// get all users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-    console.error(error);
-  }
-});
-
-// get an user by the id
-app.get("/users/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const user = await User.findById(id);
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-    console.error(error);
-  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
